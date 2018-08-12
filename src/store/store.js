555d9f3f754652e97654b6be4cf95b8fed472f89
Jakub Semik <kuba.semik@gmail.com>
2018-08-12 11:19:07 +0200

    Added react-persist (#6)

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,20 @@
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['JWT'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(
+  persistedReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+export const persistor = persistStore(store);
 
 export default store;

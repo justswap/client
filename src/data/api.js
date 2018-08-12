c5049f30a4988a73cd64c182ebe712c537bf9d58
Jakub Semik <kuba.semik@gmail.com>
2018-08-12 10:03:06 +0200

    Add Authorization header on each request

--- src/data/api.js
+++ src/data/api.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import store from '../store/store';
 
-export default axios.create({
+const api = axios.create({
   baseURL: `http://127.0.0.1:8000/`
 });
+
+api.interceptors.request.use(
+  config => {
+    const JWT = store.getState().JWT;
+    if (JWT) {
+      config.headers.Authorization = `Authorization: JWT ${JWT}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,13 +1,13 @@
 import { SET_JWT } from './constants';
 
 const initialState = {
-  jwt: ''
+  JWT: ''
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_JWT:
-      return { ...state, jwt: action.payload };
+      return { ...state, JWT: action.payload };
     default:
       return state;
   }
